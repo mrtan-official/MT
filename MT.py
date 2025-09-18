9b2ab2b640a7782384b65482b92a3e505d206ff8
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-09-18 10:39:47 +0600

    Update MT.py

--- MT.py
+++ MT.py
@@ -1,8 +1,6 @@
 import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
-print('\x1b[38;5;208mNote \033[1;37m:\x1b[38;5;156m When setup is done, You see SetUp done msg')
-time.sleep(3)
 bit = platform.architecture()[0]
 if bit == '64bit':
     print('\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m You are 64 Bit user')
