796eb739924a0f12b32998cd6f3571a3486bcd75
MR-TAN <135997970+mrtan-official@users.noreply.github.com>
2025-09-26 23:53:33 +0600

    Update MT.py

--- MT.py
+++ MT.py
@@ -2,6 +2,7 @@ import os, platform, time, sys
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Checking For Update...')
 os.system('git pull')
 print('\n\033[1;31m[\x1b[38;5;156m●\033[1;31m]\x1b[38;5;156m Follow My Github')
+time.sleep(2)
 os.system('xdg-open https://github.com/mrtan-official')
 bit = platform.architecture()[0]
 if bit == '64bit':
@@ -12,4 +13,5 @@ if bit == '64bit':
     #while True:
        # print('                          Tool is off')
 else:
-    print("You are not 64bit")
+    print("32bit tool is not available now.\nPlease contact admin")
+    os.system("xdg-open https://facebook.com/MrT4n.official")
